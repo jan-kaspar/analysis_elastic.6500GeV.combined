9b87db8ea40623fd5f10cb95716c8a4520bdc9f9
Jan Kaspar <jan.kaspar@cern.ch>
2017-10-23 19:22:42 +0200

    Also builds graphs as function of normalisation bias.

--- coulomb_analysis_1/simulation/process_fits.cc
+++ coulomb_analysis_1/simulation/process_fits.cc
@@ -1,6 +1,7 @@
 #include "TH1D.h"
 #include "TFile.h"
 #include "TGraph.h"
+#include "TGraphErrors.h"
 
 #include "../../stat.h"
 
@@ -11,48 +12,20 @@ using namespace std;
 
 //----------------------------------------------------------------------------------------------------
 
-int main()
+void ProcessOneGroup(const string &base_dir,
+		const vector<string> &models, const vector<string> &unc_modes, const vector<string> norms,
+		const vector<string> binnings, unsigned int seed_min, unsigned int seed_max, const vector<string> t_maxs,
+		TFile *f_in_model, TFile *f_out)
 {
-	// settings
-	vector<string> models = {
-		"exp1,rho=0.10",
-		"exp1,rho=0.14",
-		"exp3,rho=0.10",
-		"exp3,rho=0.14",
-	};
+	printf("\n>> %s\n", base_dir.c_str());
 
-	vector<string> unc_modes = {
-		"stat",
-		"stat+syst",
-		"stat+syst+norm",
-	};
+	TDirectory *d_base = f_out->mkdir(base_dir.c_str());
 
-	vector<string> norms = {
-		"0.00",
-	};
-
-	vector<string> binnings = {
-		"ob-2-10-0.05",
-	};
-
-	unsigned int seed_min = 1, seed_max = 100;
-
-	vector<string> t_maxs = {
-		"0.15"
-	};
-
-	// open model file
-	TFile *f_in_model = TFile::Open("build_models.root");
-
-	// prepare output
-	TFile *f_out = TFile::Open("process_fits.root", "recreate");
-
-	// process all
 	for (const auto &model : models)
 	{
 		printf("* %s\n", model.c_str());
 
-		TDirectory *d_model = f_out->mkdir(model.c_str());
+		TDirectory *d_model = d_base->mkdir(model.c_str());
 
 		// get model data
 		TGraph *g_model_data = (TGraph *) f_in_model->Get((model + "/g_data").c_str());
@@ -63,17 +36,23 @@ int main()
 		{
 			TDirectory *d_unc_mode = d_model->mkdir(unc_mode.c_str());
 
-			for (const auto &norm : norms)
+			for (const auto &binning : binnings)
 			{
-				TDirectory *d_norm = d_unc_mode->mkdir(("norm"+norm).c_str());
+				TDirectory *d_binning = d_unc_mode->mkdir(binning.c_str());
 
-				for (const auto &binning : binnings)
+				for (const auto &t_max : t_maxs)
 				{
-					TDirectory *d_binning = d_norm->mkdir(binning.c_str());
+					TDirectory *d_t_max = d_binning->mkdir(("tmax" + t_max).c_str());
 
-					for (const auto &t_max : t_maxs)
+					TGraphErrors *g_rho_bias_vs_norm = new TGraphErrors();
+					TGraphErrors *g_rho_rms_vs_norm = new TGraphErrors();
+					TGraphErrors *g_a_bias_vs_norm = new TGraphErrors();
+					TGraphErrors *g_a_rms_vs_norm = new TGraphErrors();
+
+					for (const auto &norm : norms)
 					{
-						TDirectory *d_t_max = d_binning->mkdir(("tmax" + t_max).c_str());
+						TDirectory *d_norm = d_t_max->mkdir(("norm"+norm).c_str());
+						gDirectory = d_norm;
 
 						TH1D *h_de_rho = new TH1D("h_de_rho", "", 20, -0.03, +0.03);
 						TH1D *h_de_a = new TH1D("h_de_a", "", 20, -60., +60.);
@@ -86,7 +65,7 @@ int main()
 							string fitDir = model.substr(0, 4) + "," + unc_mode + "," + t_max;
 
 							char file[500];
-							sprintf(file, "simu-fit-study/%s/seed%u/%s/fit.root", simuDir.c_str(), seed, fitDir.c_str());
+							sprintf(file, "%s/%s/seed%u/%s/fit.root", base_dir.c_str(), simuDir.c_str(), seed, fitDir.c_str());
 							TFile *f = TFile::Open(file);
 							TGraph *g_fit_data = (TGraph *) f->Get("g_fit_data");
 
@@ -102,7 +81,7 @@ int main()
 							delete f;
 						}
 
-						gDirectory = d_t_max;
+						gDirectory = d_norm;
 
 						h_de_rho->Write();
 						h_de_a->Write();
@@ -113,10 +92,125 @@ int main()
 						g_data->SetPoint(2, stat.GetMean(1), stat.GetMeanUnc(1));
 						g_data->SetPoint(3, stat.GetStdDev(1), stat.GetStdDevUnc(1));
 						g_data->Write("g_data");
+
+						const int idx = g_rho_bias_vs_norm->GetN();
+						const double de_norm = atof(norm.c_str());
+
+						g_rho_bias_vs_norm->SetPoint(idx, de_norm, stat.GetMean(0));
+						g_rho_bias_vs_norm->SetPointError(idx, 0., stat.GetMeanUnc(0));
+
+						g_rho_rms_vs_norm->SetPoint(idx, de_norm, stat.GetStdDev(0));
+						g_rho_rms_vs_norm->SetPointError(idx, 0., stat.GetStdDevUnc(0));
+
+						g_a_bias_vs_norm->SetPoint(idx, de_norm, stat.GetMean(1));
+						g_a_bias_vs_norm->SetPointError(idx, 0., stat.GetMeanUnc(1));
+
+						g_a_rms_vs_norm->SetPoint(idx, de_norm, stat.GetStdDev(1));
+						g_a_rms_vs_norm->SetPointError(idx, 0., stat.GetStdDevUnc(1));
 					}
+
+					gDirectory = d_t_max;
+					g_rho_bias_vs_norm->Write("g_rho_bias_vs_norm");
+					g_rho_rms_vs_norm->Write("g_rho_rms_vs_norm");
+					g_a_bias_vs_norm->Write("g_a_bias_vs_norm");
+					g_a_rms_vs_norm->Write("g_a_rms_vs_norm");
 				}
 			}
 		}
+	}
+}
+
+//----------------------------------------------------------------------------------------------------
+
+int main()
+{
+
+	// open model file
+	TFile *f_in_model = TFile::Open("build_models.root");
+
+	// prepare output
+	TFile *f_out = TFile::Open("process_fits.root", "recreate");
+
+	// process fit study
+	{
+		// settings
+		string base_dir = "simu-fit-study";
+
+		vector<string> models = {
+			"exp1,rho=0.10",
+			"exp1,rho=0.14",
+			"exp3,rho=0.10",
+			"exp3,rho=0.14",
+		};
+
+		vector<string> unc_modes = {
+			"stat",
+			"stat+syst",
+			"stat+syst+norm",
+		};
+
+		vector<string> norms = {
+			"0.00",
+		};
+
+		vector<string> binnings = {
+			"ob-2-10-0.05",
+		};
+
+		unsigned int seed_min = 1, seed_max = 100;
+
+		vector<string> t_maxs = {
+			"0.15"
+		};
+
+		gDirectory = f_out;
+		ProcessOneGroup(base_dir, models, unc_modes, norms, binnings, seed_min, seed_max, t_maxs, f_in_model, f_out);
+	}
+
+	// process normalisation study
+	{
+		// settings
+		string base_dir = "simu-norm-study";
+
+		vector<string> models = {
+			"exp1,rho=0.06",
+			"exp1,rho=0.10",
+			"exp1,rho=0.14",
+			"exp3,rho=0.06",
+			"exp3,rho=0.10",
+			"exp3,rho=0.14",
+		};
+
+		vector<string> unc_modes = {
+			"none",
+		};
+
+		vector<string> norms = {
+			"+0.10",
+			"+0.08",
+			"+0.06",
+			"+0.04",
+			"+0.02",
+			"0.00",
+			"-0.02",
+			"-0.04",
+			"-0.06",
+			"-0.08",
+			"-0.10"
+		};
+
+		vector<string> binnings = {
+			"ob-2-10-0.05",
+		};
+
+		unsigned int seed_min = 1, seed_max = 1;
+
+		vector<string> t_maxs = {
+			"0.15"
+		};
+
+		gDirectory = f_out;
+		ProcessOneGroup(base_dir, models, unc_modes, norms, binnings, seed_min, seed_max, t_maxs, f_in_model, f_out);
 	}
 
 	// clean up
